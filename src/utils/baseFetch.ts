c30d4aca3940dfc5bf8050ed77fc03595735a5b4
lixiong <346959251@qq.com>
2020-01-03 00:25:01 +0800

    mixConf

--- src/utils/baseFetch.ts
+++ src/utils/baseFetch.ts
@@ -3,7 +3,11 @@ interface IObject {
   [propName: string]: string
 }
 
-type credentials = 'include' | 'same-origin' | 'omit'
+type TCredentials = 'include' | 'same-origin' | 'omit'
+
+type TBody = string | {
+  [propName: string]: string | Blob
+}
 
 export interface IOptions {
   readonly method?: string;
@@ -11,7 +15,7 @@ export interface IOptions {
   readonly timeout?: number;
   readonly headers?: IObject;
   readonly body?: any;
-  readonly credentials?: credentials
+  readonly credentials?: TCredentials
 }
 
 export default class BaseFetch {
@@ -21,39 +25,76 @@ export default class BaseFetch {
   }
 
   async toFetch(api: string, conf: IOptions) {
-    const { url, config } = this.mixConf(api, conf)
-    try {
-      const res = await fetch(url, config)
-      const json = await res.json()
-      return json
-    } catch (reson) {
-      // reject(reson)
-      throw reson
-    }
+    // const { url, config } = this.mixConf(api, conf)
+    // try {
+    //   const res = await fetch(url, config)
+    //   const json = await res.json()
+    //   return json
+    // } catch (reson) {
+    //   // reject(reson)
+    //   throw reson
+    // }
   }
 
 
   mixConf(api: string, conf: IOptions) {
     const { options } = this
-    const { method, baseUrl, headers, body }: IOptions = { ...options, ...conf }
-    const url = /^https?:\/\//.test(api) ? api : this.joinPath(baseUrl || '', api)
-    if (method && method.toLocaleLowerCase() === 'get') {
-      return {
-        url
+    const { method = 'get', baseUrl, headers, body }: IOptions = { ...options, ...conf }
+    const localeMethod = method.toLocaleLowerCase()
+    let url = /^https?:\/\//.test(api) ? api : this.joinPath(baseUrl || '', api)
+    let tempBody: any = body
+    let result: IObject | { headers: IObject } = {
+      method: method.toUpperCase(),
+      url
+    }
+    if (typeof headers !== 'undefined') {
+      result = { ...result, headers }
+    }
+    if (localeMethod === 'get') {
+      result = { ...result, url: `${url}${url.includes('?') ? '&' : '?'}${this.queryStringify(body)}` }
+      tempBody = null
+    }
+    if (typeof body === 'object') {
+      const tempHeaders = headers || {}
+      const key = Object.keys(tempHeaders).find(key => key.toLocaleLowerCase() === 'content-type') || 'application/x-www-form-urlencoded'
+      if (tempHeaders[key].includes('x-www-form-urlencoded')) {
+        tempBody = this.queryStringify(body)
+      }
+      if (tempHeaders[key].includes('application/json')) {
+        tempBody = JSON.stringify(body)
+      }
+      if (tempHeaders[key].includes('multipart/form-data') && !(body instanceof FormData)) {
+        tempBody = this.jsonToFormData(body)
       }
     }
-    return {
-      url: '', config: {}
+
+    if (tempBody) {
+      result = { ...result, body: tempBody }
     }
+
+    return headers ? { ...result, headers } : result
   }
 
   joinPath(...paths: string[]) {
     return paths.map(p => p.replace(/\/+$/, '')).join('/')
   }
 
-  getParams(body: string | IObject) {
-
+  queryStringify(body: TBody | IObject = '') {
+    if (typeof body === 'string') {
+      return body
+    }
+    return Object.keys(body).map(key => `${key}=${typeof body[key] === 'object' ? JSON.stringify(body[key]) : body[key]}`).join('&')
   }
 
+  jsonToFormData(body: TBody = '') {
+    if (typeof body === 'string') {
+      return body
+    }
+    const tempFormData = new FormData()
+    Object.keys(body).forEach(key => {
+      tempFormData.append(key, body[key])
+    })
+    return tempFormData
+  }
 }
 
